e815bdf337a40797279b5f441ebc071eff444053
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-05-25 16:19:53 +0100

    Make app feel faster by pre-resuming the camera

--- lib/result.dart
+++ lib/result.dart
@@ -1,3 +1,4 @@
+import 'package:fast_barcode_scanner/fast_barcode_scanner.dart';
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatefulWidget {
@@ -22,6 +23,7 @@ class _ResultPageState extends State<ResultPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: const BackToScanning(),
         title: Text(widget.title),
       ),
       body: Center(
@@ -43,6 +45,29 @@ class _ResultPageState extends State<ResultPage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class BackToScanning extends StatelessWidget {
+  const BackToScanning({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Listener(
+      onPointerDown: (event) {
+        // When the user presses the button down, the camera wil resume
+        // When the user releases the button, the screen change animation starts
+        // This gives the camera to resume
+        // So there is little to no time the user is staring at a frozen camera
+        // This should make the app feel faster
+        CameraController.instance.resumeDetector();
+      },
+      child: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () async {
+            Navigator.pop(context);
+          }),
     );
   }
 }
